2a19262fe97ec17e5d91e2c9cf2f2c169944b723
xinliang Sun <38070840+xinliangSun@users.noreply.github.com>
2023-07-29 22:59:55 +0800

    Update train_eval.py

--- PSGCN/train_eval.py
+++ PSGCN/train_eval.py
@@ -1,32 +1,38 @@
 import torch
 
+from sklearn import metrics
 from torch.optim import Adam
 from torch_geometric.data import DataLoader
-from sklearn import metrics
 
 
-def train_multiple_epochs(train_dataset, test_dataset, model, args):
+def train_epochs(train_dataset, test_dataset, model, args):
 
-    train_loader = DataLoader(train_dataset, args.batch_size, shuffle=True, num_workers=2)
+    num_workers = 2
+    train_loader = DataLoader(train_dataset, args.batch_size, shuffle=True,
+                              num_workers=num_workers)
 
-    test_size = 1024  # load all test dataset
+    test_size = 1024
     test_loader = DataLoader(test_dataset, test_size, shuffle=False,
-                             num_workers=2)
+                             num_workers=num_workers)
 
     model.to(args.device).reset_parameters()
     optimizer = Adam(model.parameters(), lr=args.lr)
-
     start_epoch = 1
     pbar = range(start_epoch, args.epochs + start_epoch)
-
+    best_epoch, best_auc, best_aupr = 0, 0, 0
     for epoch in pbar:
         train_loss = train(model, optimizer, train_loader, args.device)
-
         if epoch % args.valid_interval == 0:
-            roc_auc, aupr = evaluate_metric(model, test_loader, args.device)
-
+            roc_auc, aupr = evaluate_metric(model, test_loader, args.device, epoch)
             print("epoch {}".format(epoch), "train_loss {0:.4f}".format(train_loss),
                   "roc_auc {0:.4f}".format(roc_auc), "aupr {0:.4f}".format(aupr))
+            if roc_auc > best_auc:
+                best_epoch, best_auc, best_aupr = epoch, roc_auc, aupr
+  
+
+    print("best_epoch {}".format(best_epoch), "best_auc {0:.4f}".format(best_auc), "aupr {0:.4f}".format(best_aupr))
+   
+    return best_auc, best_aupr
 
 
 def num_graphs(data):
@@ -50,11 +56,10 @@ def train(model, optimizer, loader, device):
         total_loss += loss.item() * num_graphs(data)
         optimizer.step()
         torch.cuda.empty_cache()
-
     return total_loss / len(loader.dataset)
 
 
-def evaluate_metric(model, loader, device):
+def evaluate_metric(model, loader, device, epoch):
     model.eval()
     pbar = loader
     roc_auc, aupr = None, None
@@ -66,10 +71,12 @@ def evaluate_metric(model, loader, device):
         y_true = data.y.view(-1).cpu().tolist()
         y_score = out.cpu().numpy().tolist()
 
-        fpr, tpr, threshold = metrics.roc_curve(y_true, y_score)
+        fpr, tpr, _ = metrics.roc_curve(y_true, y_score)
         roc_auc = metrics.auc(fpr, tpr)
 
-        aupr = metrics.average_precision_score(y_true, y_score)
+        precision, recall, _ = metrics.precision_recall_curve(y_true, y_score)
+        aupr = metrics.auc(recall, precision)
         torch.cuda.empty_cache()
+  
 
     return roc_auc, aupr
